a6a0c6b5f93f94e871493f1064229166e80dfc49
Ream <reamtr25@gmail.com>
2023-02-12 16:04:16 +0300

    Описание функции ввода строк в массив

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string [] SetArrayStr(int strNum)
+{
+    string [] result = new string [strNum];
+    for (int i = 0; i < strNum; i++)
+    {
+        Console.WriteLine($"Введите {i+1} строку");
+        result[i] = Convert.ToString(Console.ReadLine());
+    }
+    return result;
+}
+
